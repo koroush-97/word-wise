3b58f7871d6419231123460be2ad8fd5eed21fa8
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-08 18:17:47 +0330

    wrote Home route : Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
       </Routes>
