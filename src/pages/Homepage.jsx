c8049bf115fc82ba058f7bc2583554ef0ce9e503
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-09 17:49:15 +0330

    set link for Homepage to pricing

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1>WorldWise</h1>
+
+      <Link to="/pricing"> Pricing </Link>
+    </div>
+  );
 }
