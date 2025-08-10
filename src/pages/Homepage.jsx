f71e832e5b1ecc03d1b7c7ea371761b2eed43a53
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-10 11:42:35 +0330

    fix bug of pagenav on Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import AppNav from "../components/AppNav";
+import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      <AppNav />
+      <PageNav />
       <section>
         <h1>
           You travel the world.
