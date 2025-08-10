b9c6eea626d0bc36cccc604ab91427b18c6c12ac
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-10 12:41:40 +0330

    add all component we need to project

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -14,7 +14,10 @@ export default function PageNav() {
           <NavLink to="/product"> product </NavLink>
         </li>
         <li>
-          <NavLink to="/login"> login </NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>
+            {" "}
+            login{" "}
+          </NavLink>
         </li>
       </ul>
     </nav>
