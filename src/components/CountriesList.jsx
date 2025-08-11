5ba8478c670870df3ab8e746ae6c175e75a3135e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-11 11:21:59 +0330

    set loop for give country

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -12,12 +12,16 @@ export default function CountriesList({ cities, isLoading }) {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
-  const countries = [];
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
 
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.id} />
+        <CountryItem country={country} />
       ))}
     </ul>
   );
