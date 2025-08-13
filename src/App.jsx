03a073fd10534c2a3027553f457520c4b921a0ac
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-13 19:26:42 +0330

    Wrap citiesProvider around of all our routes

--- src/App.jsx
+++ src/App.jsx
@@ -17,26 +17,28 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
 
-          <Route path="cities" element={<CityList />} />
+            <Route path="cities" element={<CityList />} />
 
-          <Route path="cities/:id" element={<City />} />
+            <Route path="cities/:id" element={<City />} />
 
-          <Route path="countries" element={<CountriesList />} />
+            <Route path="countries" element={<CountriesList />} />
 
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
