185fbf891276068f156ed6c0dfae494a2adec7f6
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-13 19:14:18 +0330

    replace our useeffect in cities context

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,6 @@ function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-        console.log("fetch complete...");
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,3 +1,22 @@
 import { createContext } from "react";
 
 const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        console.log("fetch complete...");
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+}
