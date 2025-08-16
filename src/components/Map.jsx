67d329a83edd5eec95fa547f7257b5f000ca2f11
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-16 22:46:46 +0330

    create a component to detect our click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,17 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -21,7 +27,7 @@ function Map() {
   );
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer} onClick={() => DetectClick()}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
@@ -43,6 +49,7 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -52,6 +59,14 @@ function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form`),
+  });
 }
 
 export default Map;
