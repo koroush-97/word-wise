304390228ea82d3a4f4d9da05ddc52329d14c9bb
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-17 18:19:20 +0330

    write condition to sure that we have geoLocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -43,9 +43,11 @@ function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type="position" onClick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
