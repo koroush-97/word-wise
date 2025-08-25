d738aa1de7cd2623557ded109903b68976f4f857
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-25 21:22:24 +0330

    create logic of reducer and actions

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,15 +1,38 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initalState = {
+  user: null,
+  isAuthenticated: false,
+};
 
-function AuthProvider({children}){
-    return <AuthContext.Provider value={} >
-        {children}
-    </AuthContext.Provider>
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("unKnown action");
+  }
 }
 
-function useAuth(){
-    const context = useContext(AuthContext)
-    if(context === undefined) throw new Error(" AuthContext was used outside AuthProvider")
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initalState
+  );
+
+  function login(email, password) {}
+
+  function logout() {}
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error(" AuthContext was used outside AuthProvider");
 }
