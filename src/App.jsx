3b72e37e35a05e0821ac9745fb99c304c961d650
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-26 00:17:23 +0330

    wrap ProtectedRoute  around  applayout

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -26,7 +27,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
 
               <Route path="cities" element={<CityList />} />
