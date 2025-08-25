e4069f90ef3db7bae78de7bac985db3060ca62b5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-25 21:12:11 +0330

    create custom hook : useAuth

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const AuthContext = createContext();
 
@@ -7,4 +7,9 @@ function AuthProvider({children}){
     return <AuthContext.Provider value={} >
         {children}
     </AuthContext.Provider>
+}
+
+function useAuth(){
+    const context = useContext(AuthContext)
+    if(context === undefined) throw new Error(" AuthContext was used outside AuthProvider")
 }
