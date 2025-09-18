ba69b0bf8e787f7a73227fce827d276dbcfab131
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 19:23:59 +0330

    remove dummy Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,17 +7,3 @@ export default function Button({ children, onClick, type }) {
     </button>
   );
 }
-
-// import styles from "./Button.module.css";
-
-// export default function Button({ children, onClick, type = "button" }) {
-//   return (
-//     <button
-//       onClick={onClick}
-//       type={type}
-//       className={`${styles.btn} ${styles[type]}`}
-//     >
-//       {children}
-//     </button>
-//   );
-// }
